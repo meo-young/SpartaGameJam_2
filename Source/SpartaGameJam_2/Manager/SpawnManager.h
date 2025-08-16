7ccbc78b0f2a5b889b2d84f5fbae2743552cade3
meo-young <meoyoung000@naver.com>
2025-08-16 16:33:43 +0900

    [Add] SpawnManager 추가

---
+++ Source/SpartaGameJam_2/Manager/SpawnManager.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "SpawnManager.generated.h"
+
+UCLASS()
+class SPARTAGAMEJAM_2_API USpawnManager : public UObject
+{
+	GENERATED_BODY()
+	
+};
