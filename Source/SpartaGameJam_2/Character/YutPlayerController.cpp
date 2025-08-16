09aaa14cdf396807bf3895f8dfe9ee9fd49e1001
meo-young <meoyoung000@naver.com>
2025-08-16 20:16:08 +0900

    [Add] PlayerController 클래스 추가

---
+++ Source/SpartaGameJam_2/Character/YutPlayerController.cpp
@@ -0,0 +1,2 @@
+#include "Character/YutPlayerController.h"
+
---
+++ Source/SpartaGameJam_2/Character/YutPlayerController.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "YutPlayerController.generated.h"
+
+UCLASS()
+class SPARTAGAMEJAM_2_API AYutPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+};
